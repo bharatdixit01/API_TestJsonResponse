666c92916ffc30dd8238e3c53cc425221daa113f
Bharat Dixit <52538175+bharatdixit01@users.noreply.github.com>
2020-03-03 17:47:49 +1300

    First commit

---
+++ src/test/java/tmSandBoxAPITest.java
@@ -0,0 +1,63 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+
+/**
+ * Test Class.
+ */
+public class tmSandBoxAPITest {
+
+    /**
+     * Validating key values in API response.
+     */
+    @Test    // Test for verifying values in response body
+    public void carbonCredits() {
+
+        // Specify base URI
+        RestAssured.baseURI =
+                "https://api.tmsandbox.co.nz/v1/Categories/6327/";
+
+        // Request object
+        RequestSpecification httpsRequest = RestAssured.given();
+
+        // Response object
+        Response response = httpsRequest.request(Method.GET,
+                "Details.json?catalogue=false");
+
+        // print response in console window
+        String responseBody = response.getBody().asString();
+        System.out.println(responseBody);
+
+        // Carbon Credit verification
+        String name = response.jsonPath().get("Name");
+
+        // Verify Name value
+        Assert.assertEquals(name, "Carbon credits");
+        System.out.println("Value of Name tag in response body is: " + name);
+
+        // Carbon Credit verification
+        boolean canRelist = response.jsonPath().get("CanRelist");
+
+        // Verify Name value
+        Assert.assertEquals(canRelist, true);
+        System.out.println("canRelist tag Expected Value: true   Actual Value: "
+                + canRelist);
+
+        int i = ((ArrayList<String>) (response.getBody().jsonPath().get("Promotions.Name")))
+                .indexOf("Gallery");
+        String desc = response.getBody().jsonPath().get("Promotions[" + i + "]."
+                + "Description").toString();
+
+        Assert.assertTrue(desc.contains("2x larger image"));
+        System.out.println("Description of Promotions of element Name "
+                + "Gallery is : '" + desc
+                + "', " + "contains searched string: '2x larger image'");
+
+        //assertThat(desc,"2x larger image");
+    }
+}
